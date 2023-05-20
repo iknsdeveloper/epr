6860ccd8e299183124dc54ccbb3cb5b446dc4072
erwinagpasa <rootaccess000@gmail.com>
2023-05-20 10:07:00 +0300

    basic authentication

--- src/SessionProvider.tsx
+++ src/SessionProvider.tsx
@@ -3,9 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 
 const Provider = ({ children, session }: any) => (
-  <SessionProvider session={session} >
-    {children}
-  </SessionProvider>
+    <SessionProvider session={session}>
+        {children}
+    </SessionProvider>
 )
 
 export default Provider;
