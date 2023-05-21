dbd10f839bd17d76104b2a7e529682ae85c3ec3e
erwinagpasa <rootaccess000@gmail.com>
2023-05-21 22:59:31 +0300

    removing css elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,12 @@
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p>
-          Domain&nbsp; {process.env.NEXTAUTH_URL}</p>
-        <p>NEXT_PUBLIC_API_URL {process.env.NEXTAUTH_URL_INTERNAL} </p>
-
-
-        <p>{process.env.NEXTAUTH_URL_INTERNAL}/api/register</p>
-        <p>Version 1.0</p>
-      </div>
-
-
-
-
+    <main>
+      <p>
+        Domain&nbsp; {process.env.NEXTAUTH_URL}</p>
+      <p>NEXT_PUBLIC_API_URL {process.env.NEXTAUTH_URL_INTERNAL} </p>
+      <p>{process.env.NEXTAUTH_URL_INTERNAL}/api/register</p>
+      <p>Version 1.0</p>
     </main>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,40 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import classes from './navbar.module.css'
 import person from '../../../public/person.jpg'
-import { AiOutlineClose } from 'react-icons/ai'
 import {signIn, signOut, useSession} from 'next-auth/react'
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false)
   const {data: session} = useSession()
 
-  const handleShowDropdown = () => setShowDropdown(prev => true)
-
-  const handleHideDropdown = () => setShowDropdown(prev => false)
-
-  const loggedIn = false
-
   return (
-    <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <h2 className={classes.left}>
-          <Link href="/">logo</Link>
+    <div>
+
+        <h2>
+          <Link href="/" className='text-red-600 text-2xl'>logo</Link>
         </h2>
-        <ul className={classes.right}>
+        <ul>
           {
             session?.user
               ? (
                 <div>
-                  <Image onClick={handleShowDropdown} src={person} width='45' height='45' alt='...' />
-                  {showDropdown && (
-                    <div className={classes.dropdown}>
-                      <AiOutlineClose className={classes.closeIcon} onClick={handleHideDropdown} />
-                      <button onClick={() => {signOut(); handleHideDropdown()}} className={classes.logout}>Logout</button>
-                      <Link onClick={handleHideDropdown} href='/' className={classes.create}>Create</Link>
-                      <Link href='/dashboard/queue'>Dashbopard</Link>
-
-                    </div>
-                  )}
+                  <Image src={person} width='45' height='45' alt='...' />
+                  <button onClick={() => {signOut(); handleHideDropdown()}}>Logout</button>
+                      <p><Link href='/'>Create</Link></p>
+                      <p><Link href='/dashboard/queue'>Dashbopard</Link></p>                    
                 </div>
               )
               : (
                 <>
-                  <button onClick={() => {signIn()}} className={classes.login}>Log in</button>
+                  <button onClick={() => {signIn()}}>Log in</button>
                   <Link href='/register'>Register</Link>
                   <Link href='/dashboard/queue'>Dashbopard</Link>
-
                 </>
               )
           }
         </ul>
       </div>
-    </div>
+
   )
 }
 
